f17d4f0275d442db924951a903436385ac19c02d
unclejesus2 <125393700+unclejesus2@users.noreply.github.com>
2024-02-22 15:34:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>1:ID - Developer</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
